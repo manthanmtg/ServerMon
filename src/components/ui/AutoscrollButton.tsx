03438e9f1a01ea2488dad5ef5aaf485f2003e429
Manthan Bidarahalli Yuvaraj <manthan.gowda@gmail.com>
2026-05-07 11:22:57 +0530

    chore: remove unused autoscroll props export
    Remove the unused AutoscrollButtonProps export while keeping the prop type local to the component. Update random selector metadata for prompts/dead_code_cleaner.md.

--- src/components/ui/AutoscrollButton.tsx
+++ src/components/ui/AutoscrollButton.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 
-export interface AutoscrollButtonProps extends Omit<
+interface AutoscrollButtonProps extends Omit<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   'onToggle'
 > {
