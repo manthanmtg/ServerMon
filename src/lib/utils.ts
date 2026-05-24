8bbef263aad78d8569c2a5724c198c53756c1efd
Manthan Bidarahalli Yuvaraj <manthan.gowda@gmail.com>
2026-05-24 12:47:58 +0000

    chore(types): allow numeric class entries in cn
    Prompt: type-safety-enforcer (type_safety_enforcer.md)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function cn(...classes: (string | undefined | null | false)[]): string {
+export function cn(...classes: Array<string | number | undefined | null | false>): string {
   return classes.filter(Boolean).join(' ');
 }
 
